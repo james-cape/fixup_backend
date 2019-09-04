e18b4498cff1cf173d8161d7afcbd31a48fb624a
Trevor Nodland <trevor.nodland@gmail.com>
2019-09-04 15:03:59 -0600

    Adds max_length to char fields and runs migrations

--- fix_up/models.py
+++ fix_up/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Contractors(models.Model):
+    name = models.CharField(null=False, max_length=255)
+    email = models.CharField(null=False, max_length=255)
+    phone_number = models.CharField(null=False, max_length=255)
+    zip = models.CharField(null=False, max_length=255)
+    specialty = models.CharField(null=False, max_length=255)
+    logo = models.CharField(null=False, max_length=255)
+    example_project_1 = models.CharField(max_length=255)
+    example_project_2 = models.CharField(max_length=255)
+
+    def __str__(self):
+        return "{} - {}".format(self.name, self.email, self.phone_number, self.zip, self.specialty, self.logo)
