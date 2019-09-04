5cb0764de60549cc3645734bf645fb10d4ec6fbe
Trevor Nodland <trevor.nodland@gmail.com>
2019-09-04 14:46:47 -0600

    Fixes database configurations, sets up test

--- fix_up/tests.py
+++ fix_up/tests.py
@@ -1,13 +1,21 @@
 from rest_framework.test import APITestCase, APIClient, APIRequestFactory
 from rest_framework.views import status
 from .models import Contractors
-from .serializers import ContractorSerializer
 
+class BaseTest(APITestCase):
+    client = APIClient()
 
+class CreateContractorsTest(BaseTest):
 
-class GetAllContractorsTest(APITestCase):
-    def setUp(self):
+    def test_it_can_create_a_contractor(self):
+        data = {
+        'name': 'Builder Bob',
+        'email': 'test@mail.com',
+        'phone_number': '111111111',
+        'zip': '80124',
+        'specialty': 'construction',
+        'logo': 'logo.jpg'
+        }
+        response = self.client.post('/api/v1/contracors', data, format='json')
 
-
-
-    def test_it_can_get_contractors:
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
