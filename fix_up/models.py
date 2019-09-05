f729e328b0efd0f26d6558dfee668b88340c6720
James Cape <james.cape@gmail.com>
2019-09-05 14:17:21 -0600

    Adds User model
    Co-authored-by: Trevor Nodland 
    <42710030+tnodland@users.noreply.github.com>

--- fix_up/models.py
+++ fix_up/models.py
@@ -12,3 +12,12 @@ class Contractor(models.Model):
 
     def __str__(self):
         return "{} - {}".format(self.name, self.email, self.phone_number, self.zip, self.category, self.logo)
+
+class User(models.Model):
+    full_name = models.CharField(null=False, max_length=255)
+    email = models.EmailField()
+    phone_number = models.CharField(null=False, max_length=255)
+    zip = models.CharField(null=False, max_length=255)
+
+    def __str__(self):
+        return "{} - {}".format(self.full_name, self.email, self.phone_number, self.zip)
