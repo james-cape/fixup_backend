c2a7c2b89965bd2db1e5affde2d197bafe72ba9f
James Cape <james.cape@gmail.com>
2019-09-07 11:15:27 -0600

    Adds to test setup to simulate a project with contractors swiped right
    Co-authored-by: Trevor Nodland 
    <42710030+tnodland@users.noreply.github.com>

--- fix_up/tests_user_sees_their_projects.py
+++ fix_up/tests_user_sees_their_projects.py
@@ -57,8 +57,33 @@ class UserSeesTheirProjectsTest(BaseTest):
         )
         project_3.save()
 
-        response = self.client.get(f'/api/v1/users/{user_1.id}/projects', format='json')
+        contractor_1 = Contractor(
+            id=1,
+            name='Mario',
+            email='mario@mail.com',
+            phone_number='111111111',
+            zip='80124',
+            category='plumbing',
+            logo='logo.jpg'
+        )
+        contractor_1.save()
 
+        contractor_project_1 = ContractorProject(
+            project=project_1,
+            contractor=contractor_1,
+            contractor_choice=0,
+            user_choice=True,
+            completed=False,
+            seen=False,
+            contractor_before_picture='picture.png',
+            contractor_after_picture='picture.png',
+            user_rating=5,
+            contractor_rating=5
+        )
+        contractor_project_1.save()
+
+        response = self.client.get(f'/api/v1/users/{user_1.id}/projects', format='json')
+        import code; code.interact(local=dict(globals(), **locals()))
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.data), 2)
         self.assertEqual(response.data[1]['title'], project_1.title)
