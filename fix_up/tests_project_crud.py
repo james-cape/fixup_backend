02f64b6130f6967f6eafea059f84b6490ad427c4
James Cape <james.cape@gmail.com>
2019-09-06 20:28:03 -0600

    Adds test for finding projects swiped right by a contractor and user-approved
    Co-authored-by: Trevor Nodland 
    <42710030+tnodland@users.noreply.github.com>

--- fix_up/tests_project_crud.py
+++ fix_up/tests_project_crud.py
@@ -3,6 +3,7 @@ from rest_framework.views import status
 from .models import Contractor
 from .models import User
 from .models import Project
+from .models import ContractorProject
 from .serializers import ContractorSerializer
 from .serializers import ProjectSerializer
 
@@ -57,133 +58,119 @@ class ProjectCrudTest(BaseTest):
         self.assertEqual(response2.data['user_before_picture'], project_2.user_before_picture)
         self.assertEqual(response2.data['user_after_picture'], None)
 
-    # def test_it_can_get_a_contractors_project_index(self):
-    #
-    #     contractor_1 = Contractor(
-    #         id=1,
-    #         name='Mario',
-    #         email='mario@mail.com',
-    #         phone_number='111111111',
-    #         zip='80124',
-    #         category='plumbing',
-    #         logo='logo.jpg'
-    #     )
-    #     contractor_1.save()
-    #
-    #     contractor_2 = Contractor(
-    #         id=2,
-    #         name='Luigi',
-    #         email='luigi@mail.com',
-    #         phone_number='222222222',
-    #         zip='80224',
-    #         category='plumbing',
-    #         logo='logo.jpg'
-    #     )
-    #     contractor_2.save()
-    #
-    #     user_1 = User(
-    #         id=1,
-    #         full_name='Princess',
-    #         email='another_castle@mail.com',
-    #         phone_number='1234566',
-    #         zip='12345'
-    #     )
-    #     user_1.save()
-    #
-    #     project_1 = Project(
-    #         id=1,
-    #         user=user_1,
-    #         title='project_numero_uno',
-    #         description='this is the first project',
-    #         category='plumbing',
-    #         user_before_picture='picture.png',
-    #         user_after_picture='picture.png'
-    #     )
-    #     project_1.save()
-    #
-    #     project_2 = Project(
-    #         id=2,
-    #         user=user_1,
-    #         title='project_numero_dos',
-    #         description='this is the second project',
-    #         category='plumbing',
-    #         user_before_picture='picture.png',
-    #         user_after_picture='picture.png'
-    #     )
-    #     project_2.save()
-    #
-    #     project_3 = Project(
-    #         id=3,
-    #         user=user_1,
-    #         title='project_numero_dos',
-    #         description='this is the second project',
-    #         category='plumbing',
-    #         user_before_picture='picture.png',
-    #         user_after_picture='picture.png'
-    #     )
-    #     project_3.save()
-    #
-    #     contractor_project_1 = ContractorProject(
-    #         project=project_1,
-    #         contractor=contractor_1,
-    #         contractor_choice=0,
-    #         user_choice=False,
-    #         completed=False,
-    #         seen=False,
-    #         contractor_before_picture='picture.png',
-    #         contractor_after_picture='picture.png',
-    #         user_rating=5,
-    #         contractor_rating=5
-    #     )
-    #     contractor_project_1.save()
-    #
-    #     contractor_project_2 = ContractorProject(
-    #         project=project_1,
-    #         contractor=contractor_2,
-    #         contractor_choice=0,
-    #         user_choice=False,
-    #         completed=False,
-    #         seen=False,
-    #         contractor_before_picture='picture.png',
-    #         contractor_after_picture='picture.png',
-    #         user_rating=5,
-    #         contractor_rating=5
-    #     )
-    #     contractor_project_2.save()
-    #
-    #     contractor_project_3 = ContractorProject(
-    #         project=project_2,
-    #         contractor=contractor_1,
-    #         contractor_choice=0,
-    #         user_choice=False,
-    #         seen=False,
-    #         completed=False,
-    #         contractor_before_picture='picture.png',
-    #         contractor_after_picture='picture.png',
-    #         user_rating=5,
-    #         contractor_rating=5
-    #     )
-    #     contractor_project_3.save()
-    #
-    #     contractor_project_4 = ContractorProject(
-    #         project=project_2,
-    #         contractor=contractor_2,
-    #         contractor_choice=0,
-    #         user_choice=False,
-    #         seen=False,
-    #         completed=False,
-    #         contractor_before_picture='picture.png',
-    #         contractor_after_picture='picture.png',
-    #         user_rating=5,
-    #         contractor_rating=5
-    #     )
-    #     contractor_project_4.save()
-    #
-    #     response1 = self.client.get('/api/v1/projects', format='json')
-    #
-    #     self.assertEqual(response1.status_code, 200)
-    #     self.assertEqual(response1.data['title'], project_1.title)
-    #     self.assertEqual(response1.data['description'], project_1.description)
-    #     self.assertEqual(response1.data['category'], project_1.category)
-    #     self.assertEqual(response1.data['user_before_picture'], project_1.user_before_picture)
-    #     self.assertEqual(response1.data['user_after_picture'], None)
+    def test_it_can_get_a_contractors_project_index(self):
+
+        contractor_1 = Contractor(
+            id=1,
+            name='Mario',
+            email='mario@mail.com',
+            phone_number='111111111',
+            zip='80124',
+            category='plumbing',
+            logo='logo.jpg'
+        )
+        contractor_1.save()
+
+        contractor_2 = Contractor(
+            id=2,
+            name='Luigi',
+            email='luigi@mail.com',
+            phone_number='222222222',
+            zip='80224',
+            category='plumbing',
+            logo='logo.jpg'
+        )
+        contractor_2.save()
+
+        user_1 = User(
+            id=1,
+            full_name='Princess',
+            email='another_castle@mail.com',
+            phone_number='1234566',
+            zip='12345'
+        )
+        user_1.save()
+
+        project_1 = Project(
+            id=1,
+            user=user_1,
+            title='project_numero_uno',
+            description='this is the first project',
+            category='plumbing',
+            user_before_picture='picture.png',
+            user_after_picture='picture.png'
+        )
+        project_1.save()
+
+        project_2 = Project(
+            id=2,
+            user=user_1,
+            title='project_numero_dos',
+            description='this is the second project',
+            category='plumbing',
+            user_before_picture='picture.png',
+            user_after_picture='picture.png'
+        )
+        project_2.save()
+
+        project_3 = Project(
+            id=3,
+            user=user_1,
+            title='project_numero_dos',
+            description='this is the second project',
+            category='plumbing',
+            user_before_picture='picture.png',
+            user_after_picture='picture.png'
+        )
+        project_3.save()
+
+        contractor_project_1 = ContractorProject(
+            project=project_1,
+            contractor=contractor_1,
+            contractor_choice=0,
+            user_choice=False,
+            completed=False,
+            seen=False,
+            contractor_before_picture='picture.png',
+            contractor_after_picture='picture.png',
+            user_rating=5,
+            contractor_rating=5
+        )
+        contractor_project_1.save()
+
+        contractor_project_2 = ContractorProject(
+            project=project_1,
+            contractor=contractor_2,
+            contractor_choice=0,
+            user_choice=False,
+            completed=False,
+            seen=False,
+            contractor_before_picture='picture.png',
+            contractor_after_picture='picture.png',
+            user_rating=5,
+            contractor_rating=5
+        )
+        contractor_project_2.save()
+
+        contractor_project_3 = ContractorProject(
+            project=project_2,
+            contractor=contractor_1,
+            contractor_choice=0,
+            user_choice=False,
+            seen=False,
+            completed=False,
+            contractor_before_picture='picture.png',
+            contractor_after_picture='picture.png',
+            user_rating=5,
+            contractor_rating=5
+        )
+        contractor_project_3.save()
+
+        response = self.client.get(f'/api/v1/Contractor/{contractor_1.id}/projects', format='json')
+        # import code; code.interact(local=dict(globals(), **locals()))
+        # self.assertEqual(response.status_code, 200)
+        # self.assertEqual(response.data['title'], project_1.title)
+        # self.assertEqual(response.data['description'], project_1.description)
+        # self.assertEqual(response.data['category'], project_1.category)
+        # self.assertEqual(response.data['user_before_picture'], project_1.user_before_picture)
+        # self.assertEqual(response.data['user_after_picture'], None)
