1c6394566d4644c4c822d10dca733092f6f2e19f
James Cape <james.cape@gmail.com>
2019-09-06 18:36:14 -0600

    Tests that two projects in the db can be accessed individually
    Co-authored-by: Trevor Nodland 
    <42710030+tnodland@users.noreply.github.com>

--- fix_up/tests_project_crud.py
+++ fix_up/tests_project_crud.py
@@ -40,7 +40,7 @@ class ProjectCrudTest(BaseTest):
         project_2.save()
 
         response1 = self.client.get(f'/api/v1/projects/{Project.objects.all()[0].id}', format='json')
-        # import code; code.interact(local=dict(globals(), **locals()))
+
         self.assertEqual(response1.status_code, 200)
         self.assertEqual(response1.data['title'], project_1.title)
         self.assertEqual(response1.data['description'], project_1.description)
@@ -48,35 +48,11 @@ class ProjectCrudTest(BaseTest):
         self.assertEqual(response1.data['user_before_picture'], project_1.user_before_picture)
         self.assertEqual(response1.data['user_after_picture'], None)
 
-# class UpdateContractorTest(BaseTest):
-#     def test_it_can_update_a_contractor(self):
-#         data = {
-#         'name': 'user_1',
-#         'email': 'user_1@mail.com',
-#         'phone_number': '111111111',
-#         'zip': '80124',
-#         'category': 'construction',
-#         'logo': 'logo.jpg'
-#         }
-#         response = self.client.post('/api/v1/Contractor/', data, format='json')
-#         self.assertEqual(Contractor.objects.all()[0].name, 'user_1')
-#         self.assertEqual(Contractor.objects.all()[0].email, 'user_1@mail.com')
-#
-#         # Tests changing just the name
-#         update_data = {
-#         'name': 'new_name_1'
-#         }
-#         update_response_1 = self.client.patch(f'/api/v1/Contractor/{Contractor.objects.all()[0].id}', update_data, format='json')
-#         self.assertEqual(Contractor.objects.all()[0].name, 'new_name_1')
-#         self.assertEqual(Contractor.objects.all()[0].email, 'user_1@mail.com')
-#         self.assertEqual(update_response_1.status_code, 200)
-#
-#         # Tests changing just the name again as well as the email
-#         update_data = {
-#         'name': 'new_name_2',
-#         'email': 'new_user_1@mail.com'
-#         }
-#         update_response_1 = self.client.patch(f'/api/v1/Contractor/{Contractor.objects.all()[0].id}', update_data, format='json')
-#         self.assertEqual(Contractor.objects.all()[0].name, 'new_name_2')
-#         self.assertEqual(Contractor.objects.all()[0].email, 'new_user_1@mail.com')
-#         self.assertEqual(update_response_1.status_code, 200)
+        response2 = self.client.get(f'/api/v1/projects/{Project.objects.all()[1].id}', format='json')
+
+        self.assertEqual(response2.status_code, 200)
+        self.assertEqual(response2.data['title'], project_2.title)
+        self.assertEqual(response2.data['description'], project_2.description)
+        self.assertEqual(response2.data['category'], project_2.category)
+        self.assertEqual(response2.data['user_before_picture'], project_2.user_before_picture)
+        self.assertEqual(response2.data['user_after_picture'], None)
