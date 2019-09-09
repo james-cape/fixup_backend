eb61d0704c26820bd4543ef48f0cc9c007bc2009
James Cape <james.cape@gmail.com>
2019-09-08 20:27:58 -0600

    Adds test for user show page data
    Co-authored-by: Trevor Nodland 
    <42710030+tnodland@users.noreply.github.com>

--- fix_up/tests_user_crud.py
+++ fix_up/tests_user_crud.py
@@ -22,3 +22,32 @@ class CreateUserTest(BaseTest):
         self.assertEqual(response.data['email'], 'user_1_email@email.com')
         self.assertEqual(response.data['phone_number'], '3333333')
         self.assertEqual(response.data['zip'], '98765')
+
+class ShowUserTest(BaseTest):
+
+    def test_it_can_return_user_data(self):
+
+        user_1 = User(
+            full_name='Princess',
+            email='this_castle@mail.com',
+            phone_number='44336',
+            zip='13335'
+        )
+        user_1.save()
+
+        user_2 = User(
+            full_name='other_Princess',
+            email='another_castle@mail.com',
+            phone_number='1234566',
+            zip='12345'
+        )
+        user_2.save()
+
+        response = self.client.get(f'/api/v1/users/{user_1.id}', data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['full_name'], user_1.full_name)
+        self.assertEqual(response.data['full_name'], user_1.full_name)
+        self.assertEqual(response.data['email'], user_1.email)
+        self.assertEqual(response.data['phone_number'], user_1.phone_number)
+        self.assertEqual(response.data['zip'], user_1.zip)
